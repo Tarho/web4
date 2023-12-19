416ab41822614a2af13b7328889ab7dd1bb1f44b
Tarho <115691708+Tarho@users.noreply.github.com>
2023-12-19 18:54:09 +0700

    Add files via upload

---
+++ exercise/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+//import Exercise1 from "./exercise1/exercise1_0/exercise1";
+//import Exerciseoneone from "../src/exercise1_1/exercise1_1";
+//import Exerciseonetwo from "../src/exercise1/exercise1_2/App";
+import Exerciseonethree from "../src/exercise1/exercise1_3/routes/route";
+function App() {
+  return (
+    <div className="App">
+      <Exerciseonethree />
+    </div>
+  );
+}
+
+export default App;
